85c65f9cb97a09383e1a85b4cc094d1f05597d66
Sahil Gupte <ovenoboyo@gmail.com>
2023-07-10 16:24:59 +0530

    Misc changes

--- patch-cargo.sh
+++ patch-cargo.sh
@@ -1,8 +1,8 @@
-echo <<<EOL
+echo "
 
-[source."https://github.com/librespot-org/librespot"]
-git = "https://github.com/librespot-org/librespot"
-replace-with = "vendored-sources"
-branch = "dev"
+[source.\"https://github.com/librespot-org/librespot\"]
+git = \"https://github.com/librespot-org/librespot\"
+replace-with = \"vendored-sources\"
+branch = \"dev\"
 
-EOL; > cargo/config
+" >> cargo/config
